af3cd08d9f0ed36292c5828f09fca7d658467e5e
yousra <yousraabid93@gmail.com>
2021-06-12 01:07:50 +0100

    commit pour creation d auth

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\CourrierController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
@@ -22,6 +23,11 @@ Route::get('/role', [RoleController::class , 'index'])->name('role');
 
 // Route::get('/home', 'HomeController@index')->name('home');
 
+
+Auth::routes();
+// $user = Auth::user();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
 
