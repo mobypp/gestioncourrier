b99db7bbd1ad623cb1a99e1027e23568754b9738
Chaymae-12 <85591750+Chaymae-12@users.noreply.github.com>
2021-06-19 01:52:08 +0200

    chaymae division service

--- routes/web.php
+++ routes/web.php
@@ -18,24 +18,29 @@ Route::get('courrier', [CourrierController::class , 'index'])->name('courrier');
 Route::get('/user', [UserController::class , 'index'])->name('user');
 Route::get('/role', [RoleController::class , 'index'])->name('role');
 
-//division
+//divisions
 Route::get('/division', [App\Http\Controllers\DivisionController::class , 'index'])->name('division');
+//ajouter division
 Route::get('/createDivision',[App\Http\Controllers\DivisionController::class,'create']);
  Route::post('division',[App\Http\Controllers\DivisionController::class,'store']);
-
+//modifier division
  Route::get('editD/{id}',[App\Http\Controllers\DivisionController::class,'edit']);
 
 Route::post('/updateD',[App\Http\Controllers\DivisionController::class,'update'])->name('update.division');
 
-//service
+//services
 Route::get('/service',[App\Http\Controllers\ServiceController::class,'index'])->name('service');
+//ajouter service
 Route::get('/createService',[App\Http\Controllers\ServiceController::class,'create']);
 Route::post('/service',[App\Http\Controllers\ServiceController::class,'store']);
-
+//modifier service
 Route::get('edit/{id}',[App\Http\Controllers\ServiceController::class,'edit']);
  Route::post('/update',[App\Http\Controllers\ServiceController::class,'update'])->name('update.service');
-
+//supprimer service 
  Route::get('/delete-service/{id}' , [App\Http\Controllers\ServiceController::class,'delete']);
+
+  //modal ma3reftch lih :) 
+ //hadak divsion ba9i kaytafficah gha l id 
 
 
 // Auth::routes();
