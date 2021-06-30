138cd7ce8d15df31c8dfe3414ad9bf9f5be37158
Moucharraf El Bani <thegeekygamer86@gmail.com>
2021-06-30 15:57:45 +0100

    fix migration

--- database/migrations/2019_09_11_082322_create_organismes_table.php
+++ database/migrations/2019_09_11_082322_create_organismes_table.php
@@ -14,7 +14,7 @@ class CreateOrganismesTable extends Migration
     public function up()
     {
         Schema::create('organismes', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             $table->string('nom');
             $table->string('localisation');
             $table->timestamps();
